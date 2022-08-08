8c818b25715ed82e73463cf8d311e8918abf8d86
AvanMyers <smail1341@gmail.com>
2022-08-08 15:18:31 +0300

    project

---
+++ change of variables/change of variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace change_of_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hamsford";
+            string surname = "Kris";
+
+            Console.WriteLine($"Привет, {name} {surname} ");
+
+            string replaceName = name;
+            name = surname;
+            surname = replaceName;
+
+            Console.Write($"Привет, {name} {surname} ");
+            Console.ReadKey();
+        }
+    }
+}
